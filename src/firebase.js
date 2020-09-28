083038214da677a8ad3cf4e2b7c984d19477dbf8
jahusby <32870250+jahusby@users.noreply.github.com>
2020-09-28 15:28:53 +0200

    commit

---
+++ src/firebase.js
@@ -0,0 +1,9 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyCIjw-k8eHsOGxY2QELaL5je7kLi-Zw0_s",
+  authDomain: "whatsapp-mern-f52e6.firebaseapp.com",
+  databaseURL: "https://whatsapp-mern-f52e6.firebaseio.com",
+  projectId: "whatsapp-mern-f52e6",
+  storageBucket: "whatsapp-mern-f52e6.appspot.com",
+  messagingSenderId: "329376044517",
+  appId: "1:329376044517:web:4a3be8d5c1b10dfff48b0c",
+};
